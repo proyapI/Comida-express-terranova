6679aa0fdd2779cd361e9b276f1d6aa2af55df49
Maria Ramirez <maramirezm@correo.udistrital.edu.co>
2020-11-24 16:19:22 -0500

    img y presentacion

--- presentacion/encabezado.php
+++ presentacion/encabezado.php
@@ -1,6 +1,6 @@
 <div class="row mt-3">
 	<div class="col-3 text-center">
-		<img src="img/logo.png" width="100px">
+		<img src="img/logo.jpg" width="100px">
 	</div>
 	<div class="col-9 text-center">
 		<h1>COMIDA EXPRESS TERRANOVA</h1>
