e4ff8e00ffb453fed2a4a22320228488a267ad5d
Maria Ramirez <maramirezm@correo.udistrital.edu.co>
2020-11-24 16:09:37 -0500

    artefactos, img y presentacion

---
+++ presentacion/sesionCliente.php
@@ -0,0 +1,18 @@
+<?php
+$cliente = new Cliente($_SESSION["id"]);
+$cliente -> consultar();
+?>
+<div class="container">
+	<div class="row mt-3">
+		<div class="col">
+			<div class="card">
+				<div class="card-header">
+					<h3>Bienvenido</h3>
+				</div>
+				<div class="card-body">
+					Cliente: <?php echo $cliente -> getNombre() . " " . $cliente -> getApellido() ?>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ presentacion/sesionDomiciliario.php
@@ -0,0 +1,18 @@
+<?php
+$domiciliario = new Domiciliario($_SESSION["id"]);
+$domiciliario -> consultar();
+?>
+<div class="container">
+	<div class="row mt-3">
+		<div class="col">
+			<div class="card">
+				<div class="card-header">
+					<h3>Bienvenido</h3>
+				</div>
+				<div class="card-body">
+					Domiciliario: <?php echo $domiciliario -> getNombre() . " " . $domiciliario -> getApellido() ?>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
